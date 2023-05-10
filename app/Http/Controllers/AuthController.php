9069ade13125c2a75d81cd05f7e59abac2a6d77c
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2023-05-10 22:17:28 +0700

    Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
 
 class AuthController extends Controller
 {
@@ -17,13 +19,31 @@ class AuthController extends Controller
         return response()->json('Ping - Running ...');
     }
 
-    public function login(): JsonResponse
+    public function login(LoginUserRequest $req): JsonResponse
     {
-        return response()->json('This is my login');
+        // return response()->json('This is my login');
+        $req->validated($req->all());
+
+        // Handle an authentication attempt.
+        if (!Auth::attempt($req->only('email', 'password'))) {
+            return $this->error([], 'Credentials do not match', 401);
+        }
+
+        $user = User::where('email', $req->email)->first();
+
+        // https://laravel.com/docs/10.x/sanctum#issuing-api-tokens
+        $token = $user->createToken('Api Token Of ' . $user->email);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ]);
+        // return response()->json('This is my login');
     }
 
     public function register(StoreUserRequest $req): JsonResponse
     {
+        // return response()->json('This is my register');
         $req->validated($req->all());
 
         $user = User::create([
